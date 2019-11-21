5dcb559b7d9d40307615c04f1c4dfd1c581a4cf5
Alex J Derbyshire <tuf90849@temple.edu>
2019-11-21 14:56:08 -0500

    first commit message

---
+++ Project4/Project4/Form1.cs
@@ -0,0 +1,162 @@
+﻿// Code behind Main Form for BookDVDCDShop Project
+// Written in 2008 by Joe Jupin
+
+// Modified April 27, 2016 by Frank Friedman   Ver 20
+// Updated 11/18/2016 by Elliot Stoner Ver 21
+// Uodated 06/17/2017 by Frank Friedman Ver 42
+// Updated 06/27/2018 by Frank Friedman Ver 50
+// Updated 06/18/2019 by Frank Friedman Ver 51
+
+// Manages all activities behind all controls on the Project form.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+// using System.IO;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using BookCDDVDShop.Classes;
+
+// To serialize a persistant object
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BookCDDVDShop
+{
+    public partial class frmBookCDDVDShop : Form
+    {
+        // This is creates and instance of an object from a class that manages a List of 
+        //    Products (DVDs, Books, Classical Music Stuff etc)		
+        ProductList thisProductList = new ProductList();
+
+        // This index keeps track of the current Owl
+        int currentIndex = -1;
+
+        int recordsProcessedCount = 0;
+        // File to read or write to
+        string FileName = "PersistentObject.bin";
+
+        // Database methods and attributes stored here
+        ProductDB dbFunctions = new ProductDB();// Parameterless Constructor for fmrEmpMan
+        public frmBookCDDVDShop()
+        {
+            InitializeComponent();
+        }  // end frmEmpMan Parameterless Constructor
+
+
+        // Tooltip messages
+        string ttCreateCDChamber = "Click to enter Make CDChamber mode to add a CDChamber to the List of Products.";
+        string ttCreateCDOrchestra = "Click to enter Make CDOrchestra mode to add a CDOrchestra to the List of Products.";
+        string ttCreateBook = "Click to enter Make Book mode to add a Book to the List of Products.";
+        string ttCreateBookCIS = "Click to enter Make BookCIS mode to add a BookCIS to the List of Products.";
+        string ttCreateDVD = "Click to enter Make DVD mode to add a DVD to the List of Products.";
+        string ttSaveCDChamber = "Click to Save a CDChamber object to the List of Products.";
+        string ttSaveCDOrchestra = "Click to Save a CDOrchestra object to the List of Products.";
+        string ttSaveBookCIS = "Click to Save a BookCIS object to the list of Products.";
+        string ttSaveBook = "Click to Save the Book object to the List of Products.";
+        string ttSaveDVD = "Click to Save the DVD to the List of Products.";
+        string ttClear = "Click to Clear Form.";
+        string ttFind = "Click to Find a Product in the List of Products.";
+        string ttDelete = "Click to Delete Product from the List of Products.";
+        string ttEdit = "Click to Edit a Product's data.";
+        string ttExit = "Click to exit application.";
+
+        // ?????????? Fix The Specs (in red) for Each Item ??????????
+
+        string ttProductUPC = "Enter a 5 digit integer - no leading zeros";
+        string ttProductPrice = "Enter dollars and cents >= 0.0. NO $. Exactly two decimal digits";
+        string ttProductTitle = "Enter a string of words (all letters) separated by blanks for any item in the shop";
+        string ttProductQuantity = "Enter any integer greater than or equal to 0";
+        string ttBookISBN = "Enter Book ISBN in format nnn-nnn)";
+        string ttBookAuthor = "Enter Book Author first and last names (all letters) separated by a blank";
+        string ttBookPages = "Enter Book page count as an integer greater than 0.";
+        string ttDVDLeadActor = "Enter DVD Lead Actor with first and last names (all letters) separated by a blank.";
+        string ttDVDReleaseDate = "Enter DVD Release Date in form mm/dd/yyyy between Jan 1 1980 and Dec 31 2019. Use date picker.";
+        string ttDVDRunTime = "Enter DVD run time in minutes. Must be a positive integer.";
+        string ttBookCISCISArea = "Enter valid CIS area of study using a drop-down menu.";
+        string ttCDClassicalLabel = "Enter any sequence of words (all letters) separated by blanks.";
+        string ttCDClassicalArtists = "Enter soloists last names separated by a blank";
+        string ttCDChamberInstrumentList = "Enter Instrument names separated by a blank";
+        string ttCDOrchestraConductor = "Enter Conductor last name with all letters and one blank or one hyphen";
+
+        //*****This section has the forms load and closing events
+
+        // This sub is called when the form is loaded
+        private void frmBookCDDVDShop_Load(System.Object sender, System.EventArgs e)
+        {
+            // Read serialized binary data file
+            SFManager.readFromFile(ref thisProductList, FileName);
+            FormController.clear(this);
+
+            // Set initial Tooltips
+            toolTip1.SetToolTip(btnCreateBookCIS, ttCreateBookCIS);
+            toolTip1.SetToolTip(btnCreateBook, ttCreateBook);
+            toolTip1.SetToolTip(btnCreateCDChamber, ttCreateCDOrchestra);
+            toolTip1.SetToolTip(btnCreateCDOrchestra, ttCreateDVD);
+            toolTip1.SetToolTip(btnCreateDVD, ttCreateCDChamber);
+
+            toolTip1.SetToolTip(btnClear, ttClear);
+            toolTip1.SetToolTip(btnDelete, ttDelete);
+            toolTip1.SetToolTip(btnEdit, ttEdit);
+            toolTip1.SetToolTip(btnFind, ttFind);
+            toolTip1.SetToolTip(btnExit, ttExit);
+
+            toolTip1.SetToolTip(txtProductUPC, ttProductUPC);
+            toolTip1.SetToolTip(txtProductPrice, ttProductPrice);
+            toolTip1.SetToolTip(txtProductQuantity, ttProductQuantity);
+            toolTip1.SetToolTip(txtProductTitle, ttProductTitle);
+            toolTip1.SetToolTip(txtCDOrchestraConductor, ttCDOrchestraConductor);
+            toolTip1.SetToolTip(txtBookISBNLeft, ttBookISBN);
+            toolTip1.SetToolTip(txtBookAuthor, ttBookAuthor);
+            toolTip1.SetToolTip(txtBookPages, ttBookPages);
+            toolTip1.SetToolTip(txtDVDLeadActor, ttDVDLeadActor);
+            toolTip1.SetToolTip(txtDVDReleaseDate, ttDVDReleaseDate);
+            toolTip1.SetToolTip(txtDVDRunTime, ttDVDRunTime);
+            toolTip1.SetToolTip(txtCDClassicalLabel, ttCDClassicalLabel);
+            toolTip1.SetToolTip(txtCDClassicalArtists, ttCDClassicalArtists);
+            toolTip1.SetToolTip(txtCDOrchestraConductor, ttCDOrchestraConductor);
+            toolTip1.SetToolTip(txtCDChamberInstrumentList, ttCDChamberInstrumentList);
+            toolTip1.SetToolTip(txtBookCISCISArea, ttBookCISCISArea);
+            toolTip1.SetToolTip(btnCreateBookCIS, ttCreateBookCIS);
+        } // end frmEBookCDDVDShop_Load
+
+
+        // Clear textboxes
+        private void btnClear_Click(System.Object sender, System.EventArgs e)
+        {
+            FormController.clear(this);
+        }  // end btnClear_Click
+
+
+        // Checks if Product List is empty and, if not, copies the data for the
+        // ith Product to the appropriate group textboxes using the display sub.
+        // Also checks to determine if the next button should be enabled.
+        private void getItem(int i)
+        {
+            if (thisProductList.Count() == 0)
+            {
+                btnDelete.Enabled = false;
+                btnEdit.Enabled = false;
+                // btnToString.Enabled = false;
+                lblUserMessage.Text = "Please select an operation";
+            }
+            else if (i < 0 || i >= thisProductList.Count())
+            {
+                MessageBox.Show("getItem error: index out of range");
+                return;
+            }
+            else
+            {
+                currentIndex = i;
+                thisProductList.getAnItem(i).Display(this);     // *****************
+                // thisOwlList.RemoveAt(i);
+                lblUserMessage.Text = "Object Type: " + thisProductList.getAnItem(i).GetType().ToString() +
+                        " List Index: " + i.ToString();
+                btnFind.Enabled = true;
+                btnDelete.Enabled = true;
+                btnEdit.Enabled = true;
+            }  // end else
+        } // end getItem
